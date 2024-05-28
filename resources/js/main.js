c833d84c71f3636af633d64d6418f17788287ba1
Tomas Yu Nakasone <tomas.nakasone@arreglatech.com>
2024-05-27 22:19:40 -0300

    Finished the game

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,17 +1,74 @@
+// Globals
 const TABLE_ELEMENTS = 8;
+let tableNumbers = [];
+let revealedCount = 0;
+let card1;
+let card2;
+let valueCard1;
+let valueCard2;
+let matches = 0;
+const matchesTxt = document.getElementById('matches');
 
 // Functions
+function hide() {
+  card1.innerHTML = `  `;
+  card2.innerHTML = `  `;
+  revealedCount = 0;
+}
+
+function match() {
+  matches++;
+  console.log('Matches', matches);
+  matchesTxt.innerHTML = `Matches: ${matches}`;
+  revealedCount = 0;
+  if (matches === TABLE_ELEMENTS) {
+    alert('You won!');
+  }
+}
+
 function reveal(index) {
-  console.log(index);
+  revealedCount++;
+
+  if (revealedCount === 1) {
+    card1 = document.getElementById(`button_${index}`);
+    valueCard1 = tableNumbers[index];
+    card1.innerHTML = `${valueCard1}`;
+    card1.disabled = true;
+  } else if (revealedCount === 2) {
+    card2 = document.getElementById(`button_${index}`);
+    valueCard2 = tableNumbers[index];
+    card2.innerHTML = `${valueCard2}`;
+    card2.disabled = true;
+
+    if (valueCard1 === valueCard2) {
+      match();
+    } else {
+      setTimeout(() => {
+        hide();
+      }, 1000);
+    }
+    card1.disabled = false;
+    card2.disabled = false;
+  }
 }
 
 function handleReset(buttons) {
-  console.log('Reset');
-  initializeGame(buttons);
+  console.log('Reset!');
+  initializeGame();
+  Array.from(buttons).map((button) => {
+    button.innerHTML = `  `;
+    button.disabled = false;
+  });
+  matches = 0;
 }
 
-function handleReveal(buttons) {
-  console.log('Reveal');
+function handleRevealAll(buttons) {
+  console.log('Reveal All!');
+  Array.from(buttons).map((button, i) => {
+    button.innerHTML = `${tableNumbers[i]}`;
+    button.disabled = true;
+  });
+  matches = TABLE_ELEMENTS;
 }
 
 /**
@@ -25,19 +82,12 @@ function generateRandomNumbers() {
     availableNumbers.push(i);
   }
 
-  return availableNumbers.sort(() => Math.random() - 0.5);
+  tableNumbers = availableNumbers.sort(() => Math.random() - 0.5);
 }
 
-function initializeGame(buttons) {
-  const availableNumbers = generateRandomNumbers();
-  Array.from(buttons).map((button, i) => {
-    button.innerHTML = `${availableNumbers[i]}`;
-  });
-}
-
-function main(buttons) {
-  console.log('Initializing game!');
-  initializeGame(buttons);
+function initializeGame() {
+  generateRandomNumbers();
+  console.log(tableNumbers);
 }
 
 // OnLoad
@@ -58,8 +108,9 @@ document.addEventListener('DOMContentLoaded', function () {
     handleReset(buttons);
   };
   revealBtn.onclick = () => {
-    handleReveal(buttons);
+    handleRevealAll(buttons);
   };
 
-  main(buttons);
+  console.log('Initializing game!');
+  initializeGame();
 });
